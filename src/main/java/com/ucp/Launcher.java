b75ec8153e3d4fab9373dbc7fd0c278acbd3a170
Mathieu Hannoun <mathieu95480@gmail.com>
2018-12-19 13:38:23 +0100

    :heavy_plus_sign: Add support for Log4J 2

--- src/main/java/com/ucp/Launcher.java
+++ src/main/java/com/ucp/Launcher.java
@@ -6,14 +6,21 @@ import com.ucp.bussiness.factories.PlaceHolderFactory;
 import com.ucp.configuration.SpringConfiguration;
 import com.ucp.hibernate.DBConnection;
 import com.ucp.hibernate.PlaceHolderDataStorable;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.springframework.context.ApplicationContext;
-
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Launcher {
+    final static Logger logger = LogManager.getLogger(Launcher.class);
+
     public static void main(String[] args) {
+        // Logger example, launcher must be instantiate
+        Launcher launcher = new Launcher();
+        logger.trace("Program initialization");
+
         // Spring basic setup example
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringConfiguration.class);
         PlaceHolderFactory placeHolderFactory = applicationContext.getBean("placeHolderFactory", PlaceHolderFactory.class);
@@ -32,8 +39,5 @@ public class Launcher {
         session.persist(placeHolderDataStorable);
         transaction.commit();
         session.close();
-
-
-
     }
 }
