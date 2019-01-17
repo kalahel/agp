34dcba049d96685584d127e46d874646bde8b7d9
Henri <drheinrich940@gmail.com>
2019-01-17 12:59:28 +0100

    :construction: finished building propositor

--- src/main/java/com/ucp/PropositionEngine/PropositionEngine.java
+++ src/main/java/com/ucp/PropositionEngine/PropositionEngine.java
@@ -42,36 +42,51 @@ public class PropositionEngine {
         return subset;
     }
 
-    private double computeStay(ArrayList<TouristicSiteDao> subset){
-
-        ArrayList<Day> days = new ArrayList<>();
-        for (int i = 0; i < subset.size(); i++) {
-            if (i % proposition.getAverageActivitiesPerDay() == 0) {
-                ArrayList<TouristicSiteDao> dailyActivities = (ArrayList<TouristicSiteDao>) subset.subList(0, proposition.getAverageActivitiesPerDay() - 1);
-                while (subset.size() > (subset.size() - dailyActivities.size())) {
-                    subset.remove(0);
-                }
-                ArrayList<Transport> transports = new ArrayList<>();
-                //TODO ADD RANDOM BETWEEN TRANSPORTATIONS
-                while (transports.size() <= dailyActivities.size()) {
-                    transports.add(new Bus(1, 1));
-                }
-                ArrayList<TouristicSite> sites = new ArrayList<>();
-                //TODO USE CONVERTER BETWEEN DAO AND BUSINESS
-                for (TouristicSiteDao site : dailyActivities) {
-                    TouristicSite tSite = site.generateTouristicSite();
-                    sites.add(tSite);
-                }
-                Day day = new Day(proposition.getAverageActivitiesPerDay(), Excursion.builder()
-                        .setTouristicSites(sites)
-                        .setTransports(transports)
-                        .computeComfort()
-                        .computePrice()
-                        .build());
-                days.add(day);
-            }
+    private Stay findValidStayFromList(ArrayList<Stay> stays){
+        double priceGap = 0.0;
+        double comfortGap = 0.0;
+        for(Stay stay : stays){
+            priceGap = proposition.getMaxBudget() - stay.getPrice();
+            comfortGap = proposition.getComfort() - stay.getComfort();
+            if(priceGap > 0 && comfortGap < 0) return stay;
         }
+        return null;
+    }
 
-        return 0.;
+    private Stay computeMostOptimalStay(ArrayList<TouristicSiteDao> subset){
+        ArrayList<Stay> stays = new ArrayList<>();
+        for(HotelDao hotel : hotels){
+
+            ArrayList<Day> days = new ArrayList<>();
+            for (int i = 0; i < subset.size(); i++) {
+                if (i % proposition.getAverageActivitiesPerDay() == 0) {
+                    ArrayList<TouristicSiteDao> dailyActivities = (ArrayList<TouristicSiteDao>) subset.subList(0, proposition.getAverageActivitiesPerDay() - 1);
+                    while (subset.size() > (subset.size() - dailyActivities.size())) {
+                        subset.remove(0);
+                    }
+                    ArrayList<Transport> transports = new ArrayList<>();
+                    //TODO ADD RANDOM BETWEEN TRANSPORTATIONS
+                    while (transports.size() <= dailyActivities.size()) {
+                        transports.add(new Bus(1, 1));
+                    }
+                    ArrayList<TouristicSite> sites = new ArrayList<>();
+                    //TODO USE CONVERTER BETWEEN DAO AND BUSINESS
+                    for (TouristicSiteDao site : dailyActivities) {
+                        TouristicSite tSite = site.generateTouristicSite();
+                        sites.add(tSite);
+                    }
+                    Day day = new Day(proposition.getAverageActivitiesPerDay(), Excursion.builder()
+                            .setTouristicSites(sites)
+                            .setTransports(transports)
+                            .hotel(hotel.generateHotel())
+                            .computeComfort()
+                            .computePrice()
+                            .build());
+                    days.add(day);
+                }
+            }
+            stays.add(Stay.builder().hotel(hotel.generateHotel()).setDays(days).computeComfort().computePrice().build());
+        }
+        return findValidStayFromList(stays);
     }
 }
