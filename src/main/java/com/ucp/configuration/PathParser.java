45fe4c3bfb4ad26c803fad794d8712ca0b7dda56
Matteo <baleditore@gmail.com>
2019-01-18 13:38:32 +0100

    :ambulance: Added default path if no agp.txt

--- src/main/java/com/ucp/configuration/PathParser.java
+++ src/main/java/com/ucp/configuration/PathParser.java
@@ -7,14 +7,14 @@ import java.io.IOException;
 public class PathParser {
     public static String parsePath() {
         BufferedReader reader;
-        String out = "";
+        String out = "D:\\Bureau\\agp\\src\\main\\resources"; // Default value
         try {
             reader = new BufferedReader(new FileReader(System.getProperty("user.home") + "/agp.txt"));
             out = reader.readLine();
             reader.close();
         } catch (IOException e) {
             System.err.println("You need to create the file agp.txt in your home repository : " + System.getProperty("user.home") + "\nRefer yourself to the REAMDE.md for details\n");
-            e.printStackTrace();
+            System.err.println("Default resource path set : " + out);
         }
         return out;
     }
